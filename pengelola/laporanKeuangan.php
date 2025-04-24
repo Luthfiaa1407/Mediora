4e4fe7a6524a2bb11d1d1c7792757ed70efdf3cc
kartikaaadn <kartikaadindakdewi@gmail.com>
2025-04-25 06:35:30 +0700

    add laporan keuangan

---
+++ pengelola/laporanKeuangan.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Penjualan Mediora</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style type="text/tailwindcss">
+        @layer utilities {
+            .font-poppins-semi-bold {
+                font-family: "Poppins-SemiBold", sans-serif;
+            }
+            .font-poppins-italic {
+                font-family: "Poppins-Italic", sans-serif;
+            }
+            .bg-conic-gradient {
+                background: conic-gradient(
+                    from 94.88deg at 50% 50%,
+                    rgba(71, 193, 191, 0.6) 22.11538404226303%,
+                    rgba(111, 217, 205, 0.6) 49.02962148189545%,
+                    rgba(219, 244, 241, 0.6) 82.54064321517944%
+                );
+            }
+        }
+    </style>
+</head>
+<body class="overflow-hidden">
+    <div class="melihat-laporan-penjualan bg-white rounded-[16px] h-[878px] relative shadow-[0px_4px_40.2px_0px_rgba(0,0,0,0.25)] overflow-hidden">
+        <!-- Background Images -->
+        <div class="group-19">
+            <img class="image-16 absolute left-[322px] top-[3px] w-[728px] h-[437.27px] object-cover" src="image-160.png" />
+            <img class="image-17 absolute left-[322px] top-[439.1px] w-[728px] h-[439.59px] object-cover" src="image-170.png" />
+        </div>
+        <div class="group-20">
+            <img class="image-162 absolute left-[1050px] top-[3px] w-[728px] h-[437.27px] object-cover" src="image-161.png" />
+            <img class="image-172 absolute left-[1050px] top-[439.1px] w-[728px] h-[439.59px] object-cover" src="image-171.png" />
+        </div>
+        <img class="image-23 absolute left-[-19px] top-[-73px] w-[1464px] h-[976px] object-cover aspect-[3/2]" src="image-230.png" />
+
+        <!-- Sidebar -->
+        <div class="frame-11 absolute left-[-7px] top-[-15px] w-[322px] h-[878px] overflow-hidden">
+            <img class="logo-aja-1 absolute left-[1408px] top-[15194px] w-[73.71px] h-[68px] object-cover aspect-[73.71/68]" src="./image/logo aja.png" />
+            <div class="hai absolute left-[111px] top-[29px] text-[rgba(131,131,131,0.94)] text-center font-poppins-italic text-[10px] tracking-[0.05em] font-[400] italic">Hai,</div>
+            <div class="kang-koent-jie absolute left-[111px] top-[39px] text-[rgba(0,0,0,0.94)] text-left font-poppins-semi-bold text-[21px] font-[600]">Kang Koent Jie</div>
+            <div class="keluar absolute left-[111px] top-[71px] text-[rgba(40,75,99,0.94)] text-left font-poppins-italic text-[8px] font-[400] italic underline">Keluar</div>
+            <div class="line-2 absolute left-[93px] top-[26px] w-[51px] h-[0px] border-t border-[rgba(0,0,0,0.5)] transform rotate-90 origin-left"></div>
+            <div class="line-3 absolute left-[1415px] top-[15273px] w-[280px] h-[0px] border-t border-[rgba(0,0,0,0.5)] transform -rotate-[0.205deg] origin-left"></div>
+
+            <!-- Navigation Menu -->
+            <div class="frame-56 absolute left-[18px] top-[116px] flex flex-col gap-[3px]">
+                <div class="frame-19 w-[273px] h-[46px] bg-[rgba(60,158,150,0)] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <img class="vector w-[15.44px] h-[10.69px]" src="./image/vector0.svg" />
+                    <div class="dashboard text-[#284b63] text-center font-poppins-semi-bold text-[14px] font-[600]">Dashboard</div>
+                </div>
+                <div class="frame-21 w-[273px] h-[46px] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <img class="group w-[15px] h-[14px]" src="./image/group0.svg" />
+                    <div class="kelola-stok-obat text-[#284b63] text-left font-poppins-semi-bold text-[14px] font-[600]">Kelola Stok Obat</div>
+                </div>
+                <div class="frame-29 w-[273px] h-[46px] bg-[#3c949e] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <div class="frame-28 w-[222px] h-[46px] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                        <img class="vector2 w-[13px] h-[12px]" src="./image/vector1.svg" />
+                        <div class="laporan-penjualan text-white text-left font-poppins-semi-bold text-[14px] font-[600]">Laporan Penjualan</div>
+                    </div>
+                </div>
+                <div class="frame-22 w-[273px] h-[46px] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <img class="qlementine-icons-faq-16 w-[16px] h-[16px] aspect-square" src="./image/logofaq.svg" />
+                    <div class="konsultasi text-[#284b63] text-left font-poppins-semi-bold text-[14px] font-[600]">Konsultasi</div>
+                </div>
+                <div class="frame-26 w-[273px] h-[46px] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <img class="vector3 w-[14px] h-[17px]" src="./image/vector2.svg" />
+                    <div class="kelola-pesanan text-[#284b63] text-left font-poppins-semi-bold text-[14px] font-[600]">Kelola Pesanan</div>
+                </div>
+                <div class="frame-27 w-[273px] h-[46px] rounded-[6px] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                    <img class="vector4 w-[13.52px] h-[12.38px]" src="./image/vector3.svg" />
+                    <div class="keluar2 text-[#284b63] text-left font-poppins-semi-bold text-[14px] font-[600]">Keluar</div>
+                </div>
+            </div>
+
+            <div class="frame-1321317545 absolute left-[8px] top-[16px] w-[93.71px] p-[10px] flex flex-col gap-[10px]">
+                <img class="logo-aja-12 w-full h-[68px] object-cover aspect-[73.71/68]" src="./image/logo aja.png" />
+            </div>
+            <div class="line-4 absolute left-0 top-[104px] w-[298px] h-[0px] border-t border-black"></div>
+        </div>
+
+        <!-- Main Content -->
+        <div class="frame-31 absolute left-[282px] top-0 w-[1118px] h-[876px]">
+            <div class="mediora-2025 absolute left-[41px] top-[786px] text-[rgba(189,189,189,0.94)] text-left font-poppins-semi-bold text-[16px] font-[600]">Mediora © 2025</div>
+
+            <!-- Report Cards -->
+            <div class="frame-8 absolute left-[59px] top-[175px] w-[225px] h-[158px] bg-conic-gradient rounded-[16px] border border-[rgba(255,255,255,0.86)] border-[0.4px] shadow-[0px_11px_47.3px_0px_rgba(0,0,0,0.43)] overflow-hidden backdrop-blur-[9px] flex flex-row flex-wrap content-start gap-[285px]">
+                <div class="frame-1321317579 absolute left-[-4px] top-[70px] w-[98px] h-[38px] p-[10px] flex flex-row gap-[10px] items-center justify-center"></div>
+                <div class="frame-1321317580 absolute left-[50px] top-[7px] w-[126px] p-[10px] flex flex-col gap-[10px]">
+                    <img class="folder-plus absolute left-[10px] top-[10px] w-[106px] h-[87px]" src="./image/folder-plus0.svg" />
+                </div>
+                <div class="frame-1321317578 absolute left-0 top-[22px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="frame-1321317628 w-[97px] h-[38px] flex flex-row gap-[10px] items-center justify-center"></div>
+                </div>
+                <div class="frame-13213175792 absolute left-[71px] top-[99px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="mingguan text-[#3c949e] text-left font-poppins-semi-bold text-[12px] font-[600]">Mingguan</div>
+                </div>
+            </div>
+
+            <div class="frame-82 absolute left-[355px] top-[176px] w-[225px] h-[158px] bg-conic-gradient rounded-[16px] border border-[rgba(255,255,255,0.86)] border-[0.4px] shadow-[0px_11px_47.3px_0px_rgba(0,0,0,0.43)] overflow-hidden backdrop-blur-[9px] flex flex-row flex-wrap content-start gap-[285px]">
+                <div class="frame-1321317579 absolute left-[-4px] top-[70px] w-[98px] h-[38px] p-[10px] flex flex-row gap-[10px] items-center justify-center"></div>
+                <div class="frame-1321317580 absolute left-[50px] top-[7px] w-[126px] p-[10px] flex flex-col gap-[10px]">
+                    <img class="folder-plus2 absolute left-[10px] top-[10px] w-[106px] h-[87px]" src="./image/folder-plus0.svg" />
+                </div>
+                <div class="frame-1321317578 absolute left-0 top-[22px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="frame-1321317628 w-[97px] h-[38px] flex flex-row gap-[10px] items-center justify-center"></div>
+                </div>
+                <div class="frame-13213175793 absolute left-[85px] top-[100px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="bulan text-[#3c949e] text-left font-poppins-semi-bold text-[12px] font-[600]">Bulan</div>
+                </div>
+            </div>
+
+            <div class="frame-83 absolute left-[665px] top-[175px] w-[225px] h-[158px] bg-conic-gradient rounded-[16px] border border-[rgba(255,255,255,0.86)] border-[0.4px] shadow-[0px_11px_47.3px_0px_rgba(0,0,0,0.43)] overflow-hidden backdrop-blur-[9px] flex flex-row flex-wrap content-start gap-[285px]">
+                <div class="frame-1321317579 absolute left-[-4px] top-[70px] w-[98px] h-[38px] p-[10px] flex flex-row gap-[10px] items-center justify-center"></div>
+                <div class="frame-1321317580 absolute left-[50px] top-[7px] w-[126px] p-[10px] flex flex-col gap-[10px]">
+                    <img class="folder-plus3 absolute left-[10px] top-[10px] w-[106px] h-[87px]" src="./image/folder-plus0.svg" />
+                </div>
+                <div class="frame-1321317578 absolute left-0 top-[22px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="frame-1321317628 w-[97px] h-[38px] flex flex-row gap-[10px] items-center justify-center"></div>
+                </div>
+                <div class="frame-13213175794 absolute left-[83px] top-[101px] p-[10px] flex flex-row gap-[10px] items-center justify-center">
+                    <div class="tahun text-[#3c949e] text-left font-poppins-semi-bold text-[12px] font-[600]">Tahun</div>
+                </div>
+            </div>
+
+            <!-- Header -->
+            <div class="frame-282 absolute left-[15px] top-0 w-[1143px] h-[89px] bg-[rgba(60,148,158,0.9)] rounded-[0px_6px_6px_0px] opacity-[0.9] p-[8px_12px] flex flex-row gap-[17px] items-center">
+                <div class="group-56 w-[420px] h-[45px]">
+                    <div class="laporan-penjualan-mediora absolute left-1/2 -translate-x-1/2 top-[16px] text-white text-center font-poppins-semi-bold text-[30px] font-[600]">Laporan Penjualan Mediora</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="line-6 absolute left-[297px] top-0 w-[1152px] h-[0px] border-t border-black transform rotate-90 origin-left"></div>
+    </div>
+</body>
+</html>
